ea50f4c6fc7d370aea2b4a40722d7a45bc4ce66d
moise <solcan_moise2014@yahoo.com>
2020-03-28 14:37:21 +0200

    US1 Create spring web project

---
+++ src/test/java/com/solcanm/bms/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solcanm.bms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
